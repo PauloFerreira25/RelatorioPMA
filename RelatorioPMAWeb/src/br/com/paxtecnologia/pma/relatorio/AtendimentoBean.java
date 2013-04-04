da1de462d3cf6e365867a76f53d4c0d3ad8fdaab
Pax <Pax@PAX-I3-02>
2013-04-04 15:24:41 -0300

    atendimento ejb

--- RelatorioPMAWeb/src/br/com/paxtecnologia/pma/relatorio/AtendimentoBean.java
+++ RelatorioPMAWeb/src/br/com/paxtecnologia/pma/relatorio/AtendimentoBean.java
@@ -33,9 +33,9 @@ public class AtendimentoBean {
 	private List<ChamadoVO> listaChamadoAberto;
 	private List<ChamadoVO> listaChamadoFechado;
 	private Integer totalChamadosAbertos;
-//	private Double totalPorcentoAbertos = 50.00;
-//	private Double totalPorcentoFechados = 33.33;
-//	private Integer totalChamadosFechados = 45;
+	private Double totalPorcentoAbertos;
+	private Double totalPorcentoFechados;
+	private Integer totalChamadosFechados;
 
 	public Integer getTotalChamadosAbertos() {
 		if (totalChamadosAbertos == null){
@@ -45,14 +45,23 @@ public class AtendimentoBean {
 	}
 
 	public Double getTotalPorcentoAbertos() {
+		if (totalPorcentoAbertos == null){
+			totalPorcentoAbertos = atendimentoEjb.getPorcentagemChamadosAbertos(idCliente, mesRelatorio);
+		}
 		return totalPorcentoAbertos;
 	}
 
 	public Double getTotalPorcentoFechados() {
+		if (totalPorcentoFechados == null){
+			totalPorcentoFechados = atendimentoEjb.getPorcentagemChamadosFechados(idCliente, mesRelatorio);
+		}
 		return totalPorcentoFechados;
 	}
 
 	public Integer getTotalChamadosFechados() {
+		if (totalChamadosFechados == null){
+			totalChamadosFechados = atendimentoEjb.getQtdeChamadosFechados(idCliente,mesRelatorio);
+		}
 		return totalChamadosFechados;
 	}
 
@@ -66,12 +75,12 @@ public class AtendimentoBean {
 
 		IndicacoresQtdVO b = new IndicacoresQtdVO();
 		b.setTexto("Número de Chamados Solucionados");
-		b.setValor(atendimentoEjb.getQtdeChamadosFechados(idCliente,mesRelatorio));
+		b.setValor(getTotalChamadosFechados());
 		listaGeral.add(b);
 
 		IndicacoresQtdVO c = new IndicacoresQtdVO();
 		c.setTexto("% de Chamados Solucionados");
-		c.setValor(atendimentoEjb.getPorcentagemChamadosAbertos(idCliente,mesRelatorio));
+		c.setValor(getTotalPorcentoFechados());
 		listaGeral.add(c);
 
 		IndicacoresQtdVO d = new IndicacoresQtdVO();
@@ -93,122 +102,30 @@ public class AtendimentoBean {
 	}
 
 	public List<SolicitantesVO> getListaSolicitante() {
-		listaSolicitante = new ArrayList<SolicitantesVO>();
-
-		SolicitantesVO a = new SolicitantesVO();
-		a.setSolictante("Marcelo");
-		a.setQtdeAberto(2);
-		a.setPorcentoAberto(20.00);
-		a.setQtdeFechado(5);
-		a.setPorcentoFechado(23.56);
-		listaSolicitante.add(a);
-
-		SolicitantesVO b = new SolicitantesVO();
-		b.setSolictante("PAX");
-		b.setQtdeAberto(7);
-		b.setPorcentoAberto(70.00);
-		b.setQtdeFechado(4);
-		b.setPorcentoFechado(58.56);
-		listaSolicitante.add(b);
-
-		SolicitantesVO c = new SolicitantesVO();
-		c.setSolictante("Sérgio");
-		c.setQtdeAberto(7);
-		c.setPorcentoAberto(70.00);
-		c.setQtdeFechado(4);
-		c.setPorcentoFechado(58.56);
-		listaSolicitante.add(c);
-
+		if(listaSolicitante == null){
+			listaSolicitante = atendimentoEjb.getListaSolicitantes();
+		}
 		return listaSolicitante;
 	}
 
 	public List<TipoChamadosVO> getListaTipoChamado() {
-		listaTipoChamado = new ArrayList<TipoChamadosVO>();
-
-		TipoChamadosVO a = new TipoChamadosVO();
-		a.setTipo("Incidente");
-		a.setQtdeAberto(16);
-		a.setPorcentoAberto(50.00);
-		a.setQtdeFechado(14);
-		a.setPorcentoFechado(54.56);
-		listaTipoChamado.add(a);
-
-		TipoChamadosVO b = new TipoChamadosVO();
-		b.setTipo("Mudança");
-		b.setQtdeAberto(1);
-		b.setPorcentoAberto(7.14);
-		b.setQtdeFechado(2);
-		b.setPorcentoFechado(4.56);
-		listaTipoChamado.add(b);
-
-		TipoChamadosVO c = new TipoChamadosVO();
-		c.setTipo("Task");
-		c.setQtdeAberto(11);
-		c.setPorcentoAberto(35.71);
-		c.setQtdeFechado(10);
-		c.setPorcentoFechado(44.56);
-		listaTipoChamado.add(c);
-
+		if(listaTipoChamado == null){
+			listaTipoChamado = atendimentoEjb.getListaTipoChamado();
+		}
 		return listaTipoChamado;
 	}
 
-	public List<ChamadoVO> getListaChamadoAberto() {
-		listaChamadoAberto = new ArrayList<ChamadoVO>();
-
-		ChamadoVO a = new ChamadoVO();
-		a.setIdChamado("VERZANI-219");
-		a.setTitulo("[oracle3] Criação relatório");
-		a.setDataAbertura("09 Jan 2013");
-		a.setStatus("Open");
-		a.setTipoChamado("Task");
-		listaChamadoAberto.add(a);
-
-		ChamadoVO b = new ChamadoVO();
-		b.setIdChamado("VERZANI-220");
-		b.setTitulo("[oracle2] Cria��o relat�rio");
-		b.setDataAbertura("12 Jan 2013");
-		b.setStatus("Open");
-		b.setTipoChamado("Task");
-		listaChamadoAberto.add(b);
-
-		ChamadoVO c = new ChamadoVO();
-		c.setIdChamado("VERZANI-221");
-		c.setTitulo("[oracle1] Cria��o relat�rio");
-		c.setDataAbertura("15 Jan 2013");
-		c.setStatus("Reopened");
-		c.setTipoChamado("Task");
-		listaChamadoAberto.add(c);
-
+	public List<ChamadoVO> getListaChamadoEmAberto() {
+		if(listaChamadoAberto == null){
+			listaChamadoAberto = atendimentoEjb.getListaChamadoEmAberto();
+		}
 		return listaChamadoAberto;
 	}
 
 	public List<ChamadoVO> getListaChamadoFechado() {
-		listaChamadoFechado = new ArrayList<ChamadoVO>();
-
-		ChamadoVO a = new ChamadoVO();
-		a.setIdChamado("VERZANI-219");
-		a.setTitulo("[oracle3] Cria��o relat�rio");
-		a.setDataAbertura("09 Jan 2013");
-		a.setStatus("Open");
-		a.setTipoChamado("Task");
-		listaChamadoFechado.add(a);
-
-		ChamadoVO b = new ChamadoVO();
-		b.setIdChamado("VERZANI-220");
-		b.setTitulo("[oracle2] Cria��o relat�rio");
-		b.setDataAbertura("12 Jan 2013");
-		b.setStatus("Open");
-		b.setTipoChamado("Task");
-		listaChamadoFechado.add(b);
-
-		ChamadoVO c = new ChamadoVO();
-		c.setIdChamado("VERZANI-221");
-		c.setTitulo("[oracle1] Cria��o relat�rio");
-		c.setDataAbertura("15 Jan 2013");
-		c.setStatus("Reopened");
-		c.setTipoChamado("Task");
-		listaChamadoFechado.add(c);
-
+		if(listaChamadoFechado == null){
+			listaChamadoFechado = atendimentoEjb.getListaChamadoFechado();
+		}
 		return listaChamadoFechado;
 	}
 
--- RelatoriosPMAEjb/ejbModule/br/com/paxtecnologia/pma/relatorio/ejb/AtendimentoEjb.java
+++ RelatoriosPMAEjb/ejbModule/br/com/paxtecnologia/pma/relatorio/ejb/AtendimentoEjb.java
@@ -1,10 +1,15 @@
 package br.com.paxtecnologia.pma.relatorio.ejb;
 
 import javax.ejb.Stateless;
+
+import java.util.ArrayList;
 import java.util.List;
 
 import br.com.paxtecnologia.pma.relatorio.dao.AtendimentoDAO;
+import br.com.paxtecnologia.pma.relatorio.vo.ChamadoVO;
 import br.com.paxtecnologia.pma.relatorio.vo.IndicacoresQtdVO;
+import br.com.paxtecnologia.pma.relatorio.vo.SolicitantesVO;
+import br.com.paxtecnologia.pma.relatorio.vo.TipoChamadosVO;
 
 @Stateless
 public class AtendimentoEjb {
@@ -12,18 +17,25 @@ public class AtendimentoEjb {
 	private AtendimentoDAO atendimentoDAO = new AtendimentoDAO();
 	private Integer qtdeChamadosAberto;
 	private List<IndicacoresQtdVO> listaChamadosAbertos;
+	private List<SolicitantesVO> listaSolicitante;
+	private List<TipoChamadosVO> listaTipoChamado;
+	private List<ChamadoVO> listaChamadoEmAberto;
+	private List<ChamadoVO> listaChamadoFechado;
 
-	// public Integer getQtdeChamadoAberto(Integer clienteID, String
-	// mesRelatorio){
-	// if (qtdeChamadosAberto == null){
-	// if (listaChamadosAbertos == null){
-	// // listaChamadosAbertos = getChamadosAbertos(clienteID, mesRelatorio);
-	// getChamadosAbertos(clienteID, mesRelatorio);
-	// }
-	// qtdeChamadosAberto=listaChamadosAbertos.size();
-	// }
-	// return qtdeChamadosAberto;
-	// }
+	public Integer getQtdeChamadoAberto(Integer clienteID, String mesRelatorio) {
+		if (qtdeChamadosAberto == null) {
+			if (listaChamadosAbertos == null) {
+				getChamadosAbertos(clienteID, mesRelatorio);
+			}
+			qtdeChamadosAberto = listaChamadosAbertos.size();
+		}
+		return qtdeChamadosAberto;
+	}
+
+	public List getChamadosAbertos(Integer clienteID, String mesRelatorio) {
+		
+		return listaChamadosAbertos;
+	}
 
 	public Double getPorcentagemChamadosAbertos(Integer idCliente,
 			String mesRelatorio) {
@@ -47,14 +59,141 @@ public class AtendimentoEjb {
 		return null;
 	}
 
-	public Integer getQtdeChamadosFechados(Integer idCliente, String mesRelatorio) {
+	public Integer getQtdeChamadosFechados(Integer idCliente,
+			String mesRelatorio) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	public Integer getQtdeChamadosEmAberto(Integer idCliente, String mesRelatorio) {
+	public Integer getQtdeChamadosEmAberto(Integer idCliente,
+			String mesRelatorio) {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	public Double getPorcentagemChamadosFechados(Integer idCliente,
+			String mesRelatorio) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<SolicitantesVO> getListaSolicitantes() {
+		listaSolicitante = new ArrayList<SolicitantesVO>();
+
+		SolicitantesVO a = new SolicitantesVO();
+		a.setSolictante("Marcelo");
+		a.setQtdeAberto(2);
+		a.setPorcentoAberto(20.00);
+		a.setQtdeFechado(5);
+		a.setPorcentoFechado(23.56);
+		listaSolicitante.add(a);
+
+		SolicitantesVO b = new SolicitantesVO();
+		b.setSolictante("PAX");
+		b.setQtdeAberto(7);
+		b.setPorcentoAberto(70.00);
+		b.setQtdeFechado(4);
+		b.setPorcentoFechado(58.56);
+		listaSolicitante.add(b);
+
+		SolicitantesVO c = new SolicitantesVO();
+		c.setSolictante("Sérgio");
+		c.setQtdeAberto(7);
+		c.setPorcentoAberto(70.00);
+		c.setQtdeFechado(4);
+		c.setPorcentoFechado(58.56);
+		listaSolicitante.add(c);
+		return listaSolicitante;
+	}
+
+	public List<TipoChamadosVO> getListaTipoChamado() {
+		listaTipoChamado = new ArrayList<TipoChamadosVO>();
+
+		TipoChamadosVO a = new TipoChamadosVO();
+		a.setTipo("Incidente");
+		a.setQtdeAberto(16);
+		a.setPorcentoAberto(50.00);
+		a.setQtdeFechado(14);
+		a.setPorcentoFechado(54.56);
+		listaTipoChamado.add(a);
+
+		TipoChamadosVO b = new TipoChamadosVO();
+		b.setTipo("Mudança");
+		b.setQtdeAberto(1);
+		b.setPorcentoAberto(7.14);
+		b.setQtdeFechado(2);
+		b.setPorcentoFechado(4.56);
+		listaTipoChamado.add(b);
+
+		TipoChamadosVO c = new TipoChamadosVO();
+		c.setTipo("Task");
+		c.setQtdeAberto(11);
+		c.setPorcentoAberto(35.71);
+		c.setQtdeFechado(10);
+		c.setPorcentoFechado(44.56);
+		listaTipoChamado.add(c);
+
+		return listaTipoChamado;
+	}
+
+	public List<ChamadoVO> getListaChamadoEmAberto() {
+		listaChamadoEmAberto = new ArrayList<ChamadoVO>();
+
+		ChamadoVO a = new ChamadoVO();
+		a.setIdChamado("VERZANI-219");
+		a.setTitulo("[oracle3] Criação relatório");
+		a.setDataAbertura("09 Jan 2013");
+		a.setStatus("Open");
+		a.setTipoChamado("Task");
+		listaChamadoEmAberto.add(a);
+
+		ChamadoVO b = new ChamadoVO();
+		b.setIdChamado("VERZANI-220");
+		b.setTitulo("[oracle2] Cria��o relat�rio");
+		b.setDataAbertura("12 Jan 2013");
+		b.setStatus("Open");
+		b.setTipoChamado("Task");
+		listaChamadoEmAberto.add(b);
+
+		ChamadoVO c = new ChamadoVO();
+		c.setIdChamado("VERZANI-221");
+		c.setTitulo("[oracle1] Cria��o relat�rio");
+		c.setDataAbertura("15 Jan 2013");
+		c.setStatus("Reopened");
+		c.setTipoChamado("Task");
+		listaChamadoEmAberto.add(c);
+
+		return listaChamadoEmAberto;
+	}
+
+	public List<ChamadoVO> getListaChamadoFechado() {
+		listaChamadoFechado = new ArrayList<ChamadoVO>();
+
+		ChamadoVO a = new ChamadoVO();
+		a.setIdChamado("VERZANI-219");
+		a.setTitulo("[oracle3] Cria��o relat�rio");
+		a.setDataAbertura("09 Jan 2013");
+		a.setStatus("Open");
+		a.setTipoChamado("Task");
+		listaChamadoFechado.add(a);
+
+		ChamadoVO b = new ChamadoVO();
+		b.setIdChamado("VERZANI-220");
+		b.setTitulo("[oracle2] Cria��o relat�rio");
+		b.setDataAbertura("12 Jan 2013");
+		b.setStatus("Open");
+		b.setTipoChamado("Task");
+		listaChamadoFechado.add(b);
+
+		ChamadoVO c = new ChamadoVO();
+		c.setIdChamado("VERZANI-221");
+		c.setTitulo("[oracle1] Cria��o relat�rio");
+		c.setDataAbertura("15 Jan 2013");
+		c.setStatus("Reopened");
+		c.setTipoChamado("Task");
+		listaChamadoFechado.add(c);
+
+		return listaChamadoFechado;
 	}
 
 }
