a9c017b712cb7f774956e1292268294ab20da887
Pax <Pax@PAX-I3-02>
2013-04-05 16:25:02 -0300

    workload

--- RelatoriosPMAEjb/ejbModule/br/com/paxtecnologia/pma/relatorio/ejb/WorkloadEjb.java
+++ RelatoriosPMAEjb/ejbModule/br/com/paxtecnologia/pma/relatorio/ejb/WorkloadEjb.java
@@ -8,6 +8,19 @@ import br.com.paxtecnologia.pma.relatorio.dao.WorkloadDAO;
 public class WorkloadEjb {
 	
 	private WorkloadDAO workloadDao = new WorkloadDAO();
+	
+	private String label;
+	private String tf;
+
+	public String getLabel(Integer idCliente, String mesRelatorio, Integer idGrafico, Integer idLabel) {
+		// TODO Auto-generated method stub
+		return label;
+	}
+
+	public String getTf(Integer idCliente, String mesRelatorio,Integer idGrafico, Integer idTf) {
+		// TODO Auto-generated method stub
+		return tf;
+	}
 
 
 }
