8dd7aefc00cc3571c9f0a9b390f6b4138ad62578
Pax <Pax@PAX-I3-02>
2013-04-05 15:31:12 -0300

    workload bean e graficos

--- RelatorioPMAWeb/src/br/com/paxtecnologia/pma/relatorio/WorkloadBean.java
+++ RelatorioPMAWeb/src/br/com/paxtecnologia/pma/relatorio/WorkloadBean.java
@@ -20,9 +20,9 @@ public class WorkloadBean {
 	@ManagedProperty(value = "#{clientesBean.mesRelatorio}")
 	private String mesRelatorio;
 	
-	private String nomeGrafico;
+	private String label;
 	
-	public String getTf1(Integer idCliente, String mesRelatorio, String nomeGrafico) {
+	public String getTf(Integer idCliente, String mesRelatorio, Integer idGrafico, Integer idTf) {
 		
 		return "[[1,19],[2,7],[3,28],[4,10],"
 				+ "[5,4],[6,10],[7,18],[8,16],[9,28],[10,9],"
@@ -30,21 +30,10 @@ public class WorkloadBean {
 				+ "[17,7],[18,5],[19,15],[20,12],[21,10],[22,2],[23,25]"
 				+ ",[24,19], [25,7],[26,22],[27,8],[28,1],[29,27],[30,15],[31,15]]";
 	}
-
-	public String getTf2(Integer idCliente, String mesRelatorio, String nomeGrafico) {
-		return "[[1,19],[2,7],[3,28],[4,10],"
-				+ "[5,4],[6,10],[7,18],[8,16],[9,28],[10,9],"
-				+ "[11,14],[12,15],[13,15],[14,27],[15,8],[16,11],"
-				+ "[17,7],[18,5],[19,15],[20,12],[21,10],[22,2],[23,25]"
-				+ ",[24,19], [25,7],[26,22],[27,8],[28,1],[29,27],[30,15],[31,15]]";
-	}
-
-	public String getTf3(Integer idCliente, String mesRelatorio, String nomeGrafico) {
-		return "[[1,19],[2,7],[3,28],[4,10],"
-				+ "[5,4],[6,10],[7,18],[8,16],[9,28],[10,9],"
-				+ "[11,14],[12,15],[13,15],[14,27],[15,8],[16,11],"
-				+ "[17,7],[18,5],[19,15],[20,12],[21,10],[22,2],[23,25]"
-				+ ",[24,19], [25,7],[26,22],[27,8],[28,1],[29,27],[30,15],[31,15]]";
+	
+	public String getLabel(Integer idCliente, String mesRelatorio, Integer idGrafico, Integer idLabel){
+		workloadEjb.getLabel(idCliente, mesRelatorio, idGrafico, idLabel);
+		return label;
 	}
 
 }
